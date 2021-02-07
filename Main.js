a8464e3564efee9691ff874cafee952a504487cf
tapsi <yasamingol>
2021-02-07 19:12:13 +0330

    cleaning code :)

--- Main.js
+++ Main.js
@@ -1,10 +1,11 @@
-/**********************************************Classes************************************************************/
+/**********************************************Classes***************************************************************/
 //need to convert this to a static field for account class
 let allAccounts = [];
 let allProjects = [];
 let allBids = [];
 
-//account
+//need to to convert all classes to different files
+/*********************************************Acount-Class***********************************************************/
 class Account {
     constructor(username, skills) {
         this.username = username;
@@ -23,7 +24,7 @@ function getAccountByUsername(username) {
     console.log("this user does not exist");
 }
 
-//PROJECT
+/*********************************************Projecct-Class**********************************************************/
 class Project {
     constructor(title, skills, budget) {
         this.title = title;
@@ -42,7 +43,8 @@ function getProjectByTitle(title) {
     console.log("this project does not exist");
 }
 
-//BID
+/************************************************Bid-Class***********************************************************/
+
 class Bid {
     constructor(username, projectTitle, bidAmount) {
         this.username = username;
@@ -52,12 +54,100 @@ class Bid {
     }
 }
 
-/***********************************************Functions*********************************************************/
-function calculateUserSkill(username, jobname) {
+
+/***********************************************Menus****************************************************************/
+//deserializing
+
+const prompt = require('prompt-sync')();
+console.log("Welcome to JobInja!")
+
+let commandIsValid = false;
+while (!commandIsValid) {
+    let arr = [];
+    console.log(" MENUS : \n 1.register \n 2.addProjecct \n 3.bid \n 4.auction \n 5.exit");
+    const selectedMenu = prompt("Please enter the menu number you want to enter : ");
+
+
+    if (selectedMenu === "1") {
+        console.log("\nWelcome to register menu! You can create a new account using : register <user_info> ");
+        const command = prompt("");
+        arr = command.split(" ");
+        register(arr);
+
+
+    } else if (selectedMenu === "2") {
+        console.log("welcome to  addProject menu!\nyou can add a new project using : addProject <project_info>");
+        const command = prompt("");
+        arr = command.split(" ");
+        addProject(arr);
+
+
+    } else if (selectedMenu === "3") {
+        console.log("welcome to  bid menu!\nyou can add a new bid using : bid <bid_info>");
+        const command = prompt("");
+        arr = command.split(" ");
+        addBid(arr);
+
+
+    } else if (selectedMenu === "4") {
+        console.log("welcome to  auction menu!\nyou can end auction using : auction <project_identifier>");
+
+    } else if (selectedMenu === "5") {
+        console.log("exit");
+        commandIsValid = true;
+    } else console.log("command is invalid! try again")
+
+
 }
 
-function calculateBestBid() {
+//serializing
+serializeAccounts();
+serializeProjects();
+serializeBides();
+
+
+/***********************************************Functions*********************************************************/
+function register(arr) {
+    let username = arr[1];
+    let skills = new Map;
+
+    for (let i = 2; i < arr.length; i++) {
+        let arrSkiles = [];
+        arrSkiles = arr[i].split(":");
+        skills.set(arrSkiles[0], arrSkiles[1]);
+
+    }
+    new Account(username, skills);
+    console.log("registered successfully!\n");
+
 }
+
+function addProject(arr) {
+    let title = arr[1];
+    let skills = new Map;
+    let budget = arr[arr.length - 1];
+
+    for (let i = 2; i < arr.length - 1; i++) {
+        let arrSkiles = [];
+        arrSkiles = arr[i].split(":");
+        skills.set(arrSkiles[0], arrSkiles[1]);
+    }
+    new Project(title, skills, budget);
+    console.log("project built successfully!\n");
+}
+
+function addBid(arr) {
+    let biddingUser = arr[1];
+    let projectTitle = arr[2];
+    let bidAmount = arr[3];
+    if (checkIfSkilledEnough(biddingUser, projectTitle) && checkIfBidEnough(projectTitle, bidAmount)) {
+        new Bid(biddingUser, projectTitle, bidAmount);
+        console.log("bid created successfully!\n");
+    } else {
+        console.log("cannot bid! not skilled enough.");
+    }
+}
+
 
 function checkIfSkilledEnough(userName, projectName) {
     let isSkilled = true;
@@ -77,9 +167,16 @@ function checkIfBidEnough(projectName, userBidAmount) {
     let mainBidAmount = getProjectByTitle(projectName).bidAmount;
     if (parseInt(userBidAmount) <= parseInt(mainBidAmount)) return true;
     else return false;
-
-
 }
+
+
+
+function calculateUserSkill(username, jobname) {
+}
+
+function calculateBestBid() {
+}
+
 
 function getMapSize(x) {
     let len = 0;
@@ -156,83 +253,3 @@ function deserialize() {
 
 function deserializeAllElements() {
 }
-
-/***********************************************Menus*************************************************************/
-
-const prompt = require('prompt-sync')();
-console.log("Welcome to JobInja!")
-
-let commandIsValid = false;
-while (!commandIsValid) {
-    console.log(" MENUS : \n 1.register \n 2.addProjecct \n 3.bid \n 4.auction \n 5.exit");
-    const selectedMenu = prompt("Please enter the menu number you want to enter : ");
-
-    if (selectedMenu === "1") {
-        let arr = [];
-        console.log("\nWelcome to register menu! You can create a new account using : register <user_info> ");
-        const command = prompt("");
-        arr = command.split(" ");
-        let username = arr[1];
-        let skills = new Map;
-
-        for (let i = 2; i < arr.length; i++) {
-            let arrSkiles = [];
-            arrSkiles = arr[i].split(":");
-            skills.set(arrSkiles[0], arrSkiles[1]);
-
-        }
-        new Account(username, skills);
-        console.log("registered successfully!\n");
-
-
-    } else if (selectedMenu === "2") {
-        let arr = [];
-        console.log("welcome to  addProject menu!\nyou can add a new project using : addProject <project_info>");
-        const command = prompt("");
-        arr = command.split(" ");
-        let title = arr[1];
-        let skills = new Map;
-        let budget = arr[arr.length - 1];
-
-        for (let i = 2; i < arr.length - 1; i++) {
-            let arrSkiles = [];
-            arrSkiles = arr[i].split(":");
-            skills.set(arrSkiles[0], arrSkiles[1]);
-        }
-        new Project(title, skills, budget);
-        console.log("project built successfully!\n");
-
-
-    } else if (selectedMenu === "3") {
-        let arr = [];
-        console.log("welcome to  bid menu!\nyou can add a new bid using : bid <bid_info>");
-        const command = prompt("");
-        arr = command.split(" ");
-        let biddingUser = arr[1];
-        let projectTitle = arr[2];
-        let bidAmount = arr[3];
-        if (checkIfSkilledEnough(biddingUser, projectTitle) && checkIfBidEnough(projectTitle, bidAmount)) {
-            new Bid(biddingUser, projectTitle, bidAmount);
-            console.log("bid created successfully!\n");
-        } else {
-            console.log("cannot bid! not skilled enough.");
-        }
-
-
-    } else if (selectedMenu === "4") {
-        console.log("welcome to  auction menu!\nyou can end auction using : auction <project_identifier>");
-
-    } else if (selectedMenu === "5") {
-        console.log("exit");
-        commandIsValid = true;
-    } else console.log("command is invalid! try again")
-
-
-}
-
-//serializing
-serializeAccounts();
-serializeProjects();
-serializeBides();
-
-
