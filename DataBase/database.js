663b7c429632e29b4adfb127bd556a591f82333f
tapsi <yasamingol>
2021-02-17 10:45:14 +0330

    building functions for each operation

--- DataBase/database.js
+++ DataBase/database.js
@@ -7,30 +7,22 @@ let db;
 const projectClass = require("../Classes/Project");
 let map_project1 = new Map([["C", "10"], ["C++", "20"]]);
 let project1 = new projectClass(1, "tap30", map_project1, 400, [], new Date(2022, 3, 4, 0, 0, 0), true);
-let map_project2 = new Map([["HTML", "10"], ["C++", "20"]]);
-let project2 = new projectClass(2, "snap", map_project2, 8000, [], new Date(2022, 3, 4, 0, 0, 0), true);
-
 
 
 // this is a top-level await
 (async () => {
-     db = await sqlite.open({
+    db = await sqlite.open({
         filename: 'database.db',
         driver: sqlite3.Database
     })
     await db.exec('CREATE TABLE projects (id,title, skills, budget,deadline,isAvailable)');
-    await db.run('INSERT INTO projects VALUES ("1","yasamingol","A:30","90","2020/03/04","true")');
-    await db.run('INSERT INTO projects VALUES ("2","ali","B:40","9797","2020/03/04","true")');
-    await db.run('INSERT INTO projects VALUES (?,?,?,?,?,?)',[project1.id,project1.title,project1.skills,project1.budget,project1.title,project1.isAvailable]);
-
-
-
+    await saveProjectInDB(project1.id, project1.title, project1.skills, project1.budget, project1.title, project1.isAvailable)
     console.log(await db.all('SELECT * FROM projects'))
 
 
 })()
-function saveProjectInDB(id,title, skills, budget,deadline,isAvailable){
+
+function saveProjectInDB(id, title, skills, budget, deadline, isAvailable) {
+   db.run('INSERT INTO projects VALUES (?,?,?,?,?,?)', [id, title, skills, budget, deadline, isAvailable]);
 
 }
-
-//"1","yasamingol","A:30","90","2020/03/04","true"
