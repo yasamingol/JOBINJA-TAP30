0539040dce1a785abf7dac83d3292ca97bae65a2
tapsi <yasamingol>
2021-02-28 12:19:08 +0330

    more examples of knex

--- Client/DataBase/knex.js
+++ Client/DataBase/knex.js
@@ -1,14 +1,15 @@
 const knex = require('knex')({
     client: 'sqlite',
     connection: {
-        filename: './testDB.db',
+        filename: './test.db',
     },
-    useNullAsDefault : true
+    useNullAsDefault: true
 });
 
 
 // Projects
 knex.schema
+    //Projects
     .createTable('Projects', table => {
         table.increments('id');
         table.string('title');
@@ -18,21 +19,63 @@ knex.schema
         table.integer('assignedAccountId')
 
     })
+    //Auction
+    .createTable('Auction', table => {
+        table.increments('id');
+        table.integer('projectID').references('Projects.id');
+        table.integer('winnerID')
+    })
+    //Skill
+    .createTable('Skills', table => {
+        table.increments('id');
+        table.string('skillName');
+        table.integer('skillPoint');
+        table.integer('accountID');
+        table.integer('projectID').references('Projects.id');
+    })
+    //SkillConfirmation
+    .createTable('Confirmations', table => {
+        table.increments('id');
+        table.integer('skillId').references('Skills.id');
+        table.integer('sourceAccountId');
+    })
+
+
 
     .then(() =>
-        knex('Projects').insert({title:"tapsi", budget:200, deadline:"2023/02/02", isAvailable:true, assignedAccountId:0})
+        knex('Projects').insert({
+            title: "tapsi",
+            budget: 200,
+            deadline: "2023/02/02",
+            isAvailable: true,
+            assignedAccountId: 0
+        })
     )
-    //id, title, budget,deadline,isAvailable,assignedAccountId, budget,deadline,isAvailable,assignedAccountId
+
     .then(() =>
-        knex('Projects').select('Projects.id','Projects.title','Projects.budget',
+        knex('Auction').insert({
+            projectID:0,
+            winnerID:0
+
+        })
+    )
+
+
+    .then(() =>
+        knex('Projects').select('Projects.id', 'Projects.title', 'Projects.budget',
             'Projects.deadline', 'Projects.isAvailable', 'Projects.assignedAccountId')
     )
-
     .then(rows => rows.map(row => {
-        console.log(row)
-    })
+            console.log(row)
+        })
     )
-
+    .then(() =>
+        knex('Auction').select('Auction.id', 'Auction.projectID', 'Auction.winnerID',)
+    )
+    .then(rows => rows.map(row => {
+            console.log(row)
+        })
+    )
 
     // Finally, add a .catch handler for the promise chain
     .catch(e => {
