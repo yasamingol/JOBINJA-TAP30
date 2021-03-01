af4949e70288893b0b2558691e31534aabd315a9
tapsi <yasamingol>
2021-03-01 09:32:52 +0330

    building modls with inner struct - everything is working propery

--- Client/DataBase/Models/Auction.js
+++ Client/DataBase/Models/Auction.js
@@ -1,3 +1,4 @@
 const { Model } = require('objection');
 const knex = require('/home/tapsi/IdeaProjects/concurency/knex.js')
 Model.knex(knex);
+// module.exports = Auction
--- Client/DataBase/Models/Skill.js
+++ Client/DataBase/Models/Skill.js
@@ -2,5 +2,5 @@ const { Model } = require('objection');
 const knex = require('/home/tapsi/IdeaProjects/concurency/knex.js')
 Model.knex(knex);
 
-//
+// module.exports = Skill
 
