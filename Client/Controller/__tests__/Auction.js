abb46e650662dbff3167180d5993a33e8660d89d
tapsi <yasamingol>
2021-03-08 12:37:45 +0330

    jest.fn is kind of working

--- Client/Controller/__tests__/Auction.js
+++ Client/Controller/__tests__/Auction.js
@@ -3,9 +3,11 @@ const dao = require('/home/tapsi/IdeaProjects/concurency/Client/DataBase/DAO.js'
 
 
     it('can view full project by id', () => {
-        dao.getProjectById = jest.fn().mockResolvedValueOnce({title:"tapsi",budget:200}).mockResolvedValueOnce({title:"snap",budget:50})
-        console.log(dao.getProjectById(1))
-        console.log(dao.getProjectById(1))
+        dao.getProjectById = jest.fn().mockResolvedValueOnce({title:"tapsi",budget:200})
+        let project = dao.getProjectById(1);
+        project.then(function (result){
+           console.log('result',result)
+        })
 
 
         // try {
