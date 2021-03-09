4c804718343486dc78c149f62e43426e1aa121d0
tapsi <yasamingol>
2021-03-09 11:53:58 +0330

    mocking - starting over

--- Client/Controller/__tests__/Auction.js
+++ Client/Controller/__tests__/Auction.js
@@ -5,36 +5,7 @@ const bid = new Bid(1, 1, 1, 12);
 const Project = require('../../Model/Classes/Project')
 const Account = require('../../Model/Classes/Account')
 
-//mockings...
 
-
-// dao.getProjectById = jest.fn().mockResolvedValue({id:1, title:"tapsi", budget:200, deadline:"2025/03/02",isAvailable:true ,assignedAccountId:-1 });
-// dao.getProjectSkills = jest.fn().mockResolvedValue({skillName:"C", skillPoint:20, accountID:-1, projectID:1});
-// dao.getSkillById = jest.fn().mockResolvedValueOnce({skillName:"C", skillPoint:20, accountID:-1, projectID:1})
-//     .mockResolvedValueOnce({skillName:"C", skillPoint:200, accountID:1, projectID:-1});
-// controller.getFullAccountById = jest.fn().mockResolvedValue({id:1, username:"yasamingol", password:'2431380'});
-// dao.getAccountSkills = jest.fn().mockResolvedValue({skillName:"C", skillPoint:200, accountID:1, projectID:-1});
-// dao.getAllProjectsAssignedToAnAccountUsingAccountId = jest.fn().mockResolvedValue({});
-// dao.getBidsOfProjectByProjectId = jest.fn().mockResolvedValue({id:1, userId:1, projectId:1, bidAmount:12});
-
-
-
-test('test buildFullProjectByGettingID', async () => {
-    let project = await controller.buildFullProjectByGettingID(1);
-    expect(project.skills).toEqual({skillName: "C", skillPoint: 20, accountID: -1, projectID: 1})
-})
-
-
-test('test getFullAccount', async () => {
-    let account = await controller.getFullAccountById(1);
-    expect(account.username).toEqual('yasamingol')
-})
-
-
-test('can view full project by id', async () => {
-    let project = await dao.getProjectById(1);
-    expect(project.title).toEqual("tapsi")
-});
 
 
 test('calculate userSkill', async () => {
@@ -46,8 +17,11 @@ test('calculate userSkill', async () => {
     controller.buildFullProjectByGettingID = await jest.fn().mockResolvedValue(new Project(1, 'tapsi', skills1, 200, "2025/03/02", true, -1))
     controller.buildFullAccountByGettingID = await jest.fn().mockResolvedValue(new Account(1, 'yasamingol', '2431380', skills2, null, null))
     let result = await controller.calculateUserSkill(bid);
-    console.log(result)
+    expect(result).toEqual(100188);
 });
+
+test('testing calculateBestBid')
+
 
 
 
